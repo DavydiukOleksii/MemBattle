3ab8edea5c29c51494b4ea23625961cf5bace2cb
OleksiiDavydiuk <Oleksii.Davydyuk@gmail.com>
2016-12-15 18:03:52 +0200

    first add

---
+++ Assets/Scripts/GameControlleScr.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GameControlleScr : MonoBehaviour {
+
+    public GameObject memFriendPref;
+    public GameObject memEnemyPref;
+    public Transform gameField;
+
+    // Use this for initialization
+    void Start () {
+
+        //add first mem
+        GameObject tmpMem = Instantiate(memFriendPref);
+
+        tmpMem.transform.SetParent(gameField);
+
+        tmpMem.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        tmpMem.transform.localScale = new Vector3(1, 1, 1);
+
+        tmpMem.GetComponent<OneMemScr>().armor = 2;
+        tmpMem.GetComponent<OneMemScr>().attack = 5;
+        tmpMem.GetComponent<OneMemScr>().health = 50;
+
+        tmpMem.GetComponent<OneMemScr>().id = 0;
+
+        //tmpMem.GetComponent<OneMemScr>().memImage = Resources.LoadAll(_imagePath, typeof(Texture2D));
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/OneMemScr.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OneMemScr : MonoBehaviour {
+
+    public int id;
+    public string memName;
+    public int armor;
+    public int health;
+    public int attack;
+    public int missChance;
+
+    public static int chouseID = -1;
+
+    public bool isEnemy = false;
+
+    public Sprite memImage;
+
+    void Start() {
+        transform.Find("OneMemPref").gameObject.GetComponent<Image>().sprite = memImage;
+        transform.Find("StatsTable").gameObject.transform.Find("HealthText").gameObject.GetComponent<Text>().text = health.ToString();
+        transform.Find("StatsTable").gameObject.transform.Find("AttackText").gameObject.GetComponent<Text>().text = attack.ToString();
+        transform.Find("StatsTable").gameObject.transform.Find("ArmorText").gameObject.GetComponent<Text>().text = armor.ToString();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!isEnemy && chouseID < 0) { 
+            transform.Find("OneMemPref").gameObject.GetComponent<Outline>().effectColor = new Color(0, 0, 255);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (chouseID < 0)
+        {
+            transform.Find("OneMemPref").gameObject.GetComponent<Outline>().effectColor = new Color(0, 0, 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isEnemy)
+        {
+            if (chouseID < 0)
+            {
+                transform.Find("OneMemPref").gameObject.GetComponent<Outline>().effectColor = new Color(0, 255, 0);
+                //transform.Find("SkillsTable").gameObject.
+                chouseID = id;
+            }
+            else
+            {
+                transform.Find("OneMemPref").gameObject.GetComponent<Outline>().effectColor = new Color(0, 0, 255);
+                chouseID = -1;
+            }
+        }
+    }
+}
